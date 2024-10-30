6a737b037e3da2f4ac3e09c314f5ecd9c4811221
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-30 17:12:46 +0530

    done

--- healthCare/backend/Login-app-micro-service/src/main/java/backend/com/entity/Login.java
+++ healthCare/backend/Login-app-micro-service/src/main/java/backend/com/entity/Login.java
@@ -1,5 +1,19 @@
 package backend.com.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Login {
 
+	@Id
+	@Column(name = "emailid")
+	private String emailid;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "typeofuser")
+	private String typeofuser;
 }

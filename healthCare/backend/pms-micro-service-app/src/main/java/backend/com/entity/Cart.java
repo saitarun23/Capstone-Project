0bb4f97d8a75ff23fb9c60b25b0b89eabef0c9c1
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-11-01 14:38:10 +0530

    done

--- healthCare/backend/pms-micro-service-app/src/main/java/backend/com/entity/Cart.java
+++ healthCare/backend/pms-micro-service-app/src/main/java/backend/com/entity/Cart.java
@@ -1,5 +1,10 @@
 package backend.com.entity;
 
+import lombok.Data;
+
+@Data
 public class Cart {
 
+	private String mid;
+	private int quantity;
 }
---
+++ healthCare/backend/pms-micro-service-app/src/main/java/backend/com/repository/MedicineRepository.java
@@ -0,0 +1,33 @@
+package backend.com.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import backend.com.entity.Medicine;
+
+@Repository
+public interface MedicineRepository extends JpaRepository<Medicine, Integer>{
+
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("update Medicine mm set mm.medicinename = :medicinename, mm.companyname = :companyname, "
+			+ "mm.price = :price, mm.quantity = :quantity, mm.expireDate = :expiredate where mm.mid = :mid")
+	public int updateMedicianDetails(@Param("medicinename") String medicinename, @Param("companyname") String companyName, 
+			@Param("price")  float price, @Param("quantity") int quantity, 
+			@Param("expiredate") LocalDateTime expiredate, @Param("mid") int mid);
+
+	@Query("from Medicine mm where mm.uses like %:uses% ")
+	public List<Medicine> searchMedicine(@Param("uses") String uses);
+	
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("update Medicine mm set mm.quantity = mm.quantity - :quantity where mm.mid = :mid")
+	public int updateOrderQuantity(@Param("mid") int mid,@Param("quantity") int quantity);
+}

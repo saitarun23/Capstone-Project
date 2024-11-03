9a37eb3e5f253ba589878e44f4ccd619f4bc82a3
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-11-03 12:52:38 +0530

    done

--- healthCare/frontend/front-end-app/src/app/app.module.ts
+++ healthCare/frontend/front-end-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AdmindashboardComponent,
+    UserdashboardComponent
   ],
   imports: [
     BrowserModule,

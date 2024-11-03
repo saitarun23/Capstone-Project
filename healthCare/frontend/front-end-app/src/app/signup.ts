384a62ece1a891f40dbb1a9b109a3d77dcde4785
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-11-03 23:31:10 +0530

    done

--- healthCare/frontend/front-end-app/src/app/signup.ts
+++ healthCare/frontend/front-end-app/src/app/signup.ts
@@ -1,2 +1,13 @@
 export class Signup {
+    constructor(
+        public email:string,
+        public firstname:string,
+        public lastname:string,
+        public password:string,
+        public dob:string,
+        public phonenumber:string,
+        public address:string,
+    ){
+
+    }
 }
